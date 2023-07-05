14d37512684e9d5980a52322c90f23e666b8d25b
rutyak <132219103+rutyak@users.noreply.github.com>
2023-07-05 16:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+// Array of promises with random delays
+const promises = Array.from({ length: 5 }, () => {
+  const delay = Math.floor(Math.random() * 5) + 1;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Resolved after ${delay} second(s)`);
+    }, delay * 1000);
+  });
+});
+
+// Using Promise.any() to wait for the first resolved promise
+Promise.any(promises)
+  .then((result) => {
+    document.getElementById("output").textContent = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
